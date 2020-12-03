b0ff63b7203311422031c7c426e447db325f58a4
chrispearson <cpearson@apterainc.com>
2020-12-03 09:00:25 -0500

    put old code render

--- mypaintingstore/src/App.js
+++ mypaintingstore/src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="container"><h1>Chris's Paintings</h1>
+        <div class="painting-cart">
+          <div class="customer-info">
+            <label for="email">Email</label>
+            <input type="email" id="cust-email" />
+          </div>
+          {/* <div class="cart-list">
+            [not implemented yet]
+      </div> */}
+          <div class="cart-actions">
+            <button class="btn submit-order">Order</button>
+            <div>
+            </div>
+            <section class="painting-listing">
+              <ul>
+                <li>
+                  <div class="painting-container">
+                    <div class="painting-image">
+                      <img src="https://picsum.photos/200" alt="" />
+                    </div>
+                    <div class="painting-info">
+                      <h2>Painting Name</h2>
+                      <p>painting description</p>
+                    </div>
+                    <div class="painting-action">
+                      <a href="">Add</a>
+                      <p class="counter"></p>
+                    </div>
+                  </div>
+                </li>
+                <li>
+                  <div class="painting-container">
+                    <div class="painting-image">
+                      <img src="https://picsum.photos/200" alt="" />
+                    </div>
+                    <div class="painting-info">
+                      <h2>Painting Name</h2>
+                      <p>painting description</p>
+                    </div>
+                    <div class="painting-action">
+                      <a href="">Add</a>
+                      <p class="counter"></p>
+                    </div>
+                  </div>
+                </li>
+                <li>
+                  <div class="painting-container">
+                    <div class="painting-image">
+                      <img src="https://picsum.photos/200" alt="" />
+                    </div>
+                    <div class="painting-info">
+                      <h2>Painting Name</h2>
+                      <p>painting description</p>
+                    </div>
+                    <div class="painting-action">
+                      <a href="">Add</a>
+                      <p class="counter"></p>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </section>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
